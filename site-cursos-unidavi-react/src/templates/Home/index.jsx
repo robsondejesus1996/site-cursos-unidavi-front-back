5bb57e4fce1f691b18b4df6dbc11567e647a4252
robson <robson.jesus@unidavi.edu.br>
2022-10-10 13:56:08 -0300

    Ajustes de atributos

--- site-cursos-unidavi-react/src/templates/Home/index.jsx
+++ site-cursos-unidavi-react/src/templates/Home/index.jsx
@@ -1,19 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Base } from "../Base";
 import { mockBase } from "../Base/mock";
+import {mapData} from '../../api/map-data';
+import { PageNotFound } from "../PageNotFound";
 
 function Home() {
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     const load = async () => {
-      const data = await fetch(
-        "http://localhost:1337/api/pages/?slug=lha-so-a-minha-pagina"
-      );
-      const json = await data.json();
-      console.log(json);
+
+      try{
+        const data = await fetch('http://localhost:1337/api/pages/?slug=olha-so-a-minha-pagina-1');
+        const json = await data.json();
+        const { attributes } = json.data[0];
+        const pageData = mapData([attributes]);
+        setData(pageData[0]);
+      }catch(e){
+        setData(undefined);
+      }
     };
 
     load();
   }, []);
+
+  if (data === undefined) {
+    return <PageNotFound />
+  }
+
+  if (data && !data.slug) {
+    return <h1>Carregando...</h1>
+  }
 
   return <Base {...mockBase} />;
 }
